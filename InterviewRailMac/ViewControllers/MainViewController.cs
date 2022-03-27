a685ffca2270ec3c49f62532a63c361258206b57
Alvin Fernandes <>
2022-03-27 18:36:02 +0530

    Use DynamicReactiveTableDataSource

---
+++ InterviewRailMac/ViewControllers/MainViewController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using UI.Utils;
+using System.Linq;
+
+namespace UI
+{
+
+    public partial class MainViewController : ViewControllerBase<MainViewModel>
+    {
+        public MainViewController(IntPtr handle) : base(handle)
+        {
+            ViewModel = new MainViewModel();
+            this.WhenActivated(disposables =>
+            {
+                this.WhenAnyValue(x => x.ViewModel)
+                    .Where(x => x != null)
+                    .Do(PopulateFromViewModel)
+                    .Subscribe()
+                    .DisposeWith(disposables);
+            });
+        }
+
+        private void PopulateFromViewModel(MainViewModel viewModel)
+        {
+            _stationTableView.Source = new StationTableViewDataSource(_stationTableView, viewModel.Stations);
+        }
+    }
+}
